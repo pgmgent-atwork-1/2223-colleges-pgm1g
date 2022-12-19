d85e44f0da2a87df1d7f6e4a2671607963345065
Michael Vanderpoorten <michael@yungpanda.com>
2022-12-19 14:55:07 +0100

    Week 5

---
+++ week5/params/index-app.js
@@ -0,0 +1,91 @@
+(() => {
+  const cheeses = [
+    {
+      id: "camembert",
+      name: "Camembert",
+      country: "Frankrijk",
+      shape: "cirkel",
+    },
+    {
+      id: "brie",
+      name: "Brie",
+      country: "Frankrijk",
+      shape: "driehoekig",
+    },
+    {
+      id: "gouda",
+      name: "Gouda",
+      country: "Nederland",
+      shape: "bol",
+    },
+    {
+      id: "schimmelkaas",
+      name: "Schimmelkaas",
+      country: "Frankrijk",
+      shape: "trapezium",
+    },
+    {
+      id: "geitenkaas",
+      name: "Geitenkaas",
+      country: "Spanje",
+      shape: "piramide",
+    },
+    {
+      id: "mozzarella",
+      name: "Mozzarella",
+      country: "Italië",
+      shape: "bol",
+    },
+  ];
+
+  const app = {
+    initialize() {
+      this.cacheElements();
+      this.buildUI();
+    },
+    cacheElements() {
+      this.$navigation = document.getElementById("navigation");
+      this.$detail = document.getElementById("detail");
+    },
+    buildUI() {
+      this.$navigation.innerHTML = this.generateHTMLForNavigation();
+      this.$detail.innerHTML = this.generateHTMLForDetail();
+    },
+    generateHTMLForNavigation() {
+      return cheeses
+        .map((cheese) => {
+          return `
+            <li>
+              <a href="index.html?id=${cheese.id}">${cheese.name}</a>
+            </li>`;
+        })
+        .join("");
+    },
+    generateHTMLForDetail() {
+      const params = new URLSearchParams(window.location.search);
+
+      if (!params.has("id")) {
+        return "<p>Kies een kaas</p>";
+      }
+
+      // als we hier geraken zijn we zeker dat er een parameter id is
+      const id = params.get("id");
+      const cheese = cheeses.find((cheese) => {
+        return cheese.id === id;
+      });
+
+      if (!cheese) {
+        return `<h2>Kon de kaas niet terug vinden</h2>`;
+      }
+
+      return `
+        <div>
+          <h2>${cheese.name}</h2>
+          <p>Country: ${cheese.country}</p>
+        </div>
+      `;
+    },
+  };
+
+  app.initialize();
+})();
